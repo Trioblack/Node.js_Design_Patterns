a572009b5a9a978ed8a29e88f00e8e8751d7c1b6
termosfera <termosfera@hotmail.es>
2015-03-21 11:42:54 +0100

    Adding more generalized version

--- Iterate/iterate.js
+++ Iterate/iterate.js
@@ -17,3 +17,17 @@ function finish() {
 }
 
 iterate(0);
+
+/* A more generalized version. */
+
+/**
+ * On this version we pass a collection, a function to be executed on each
+ * item and a final function to be executed when the iteration finished.
+ */
+function iterate(collection, iteratorCallback, finalCallback) {
+    Object.keys(collection).forEach(element, key, array) {
+        var item = collection[element];
+        iteratorCallback(item);
+    }
+    finalCallback();
+}
