449aff50afbc07569787ba8ffddc8f44b13c09ec
termosfera <termosfera@hotmail.es>
2015-03-21 11:17:48 +0100

    First commit

---
+++ Iterate/iterate.js
@@ -0,0 +1,18 @@
+﻿/**
+ * Function that iterates on async tasks 
+ */
+function iterate(index) {
+    if (index === tasks.length) {
+        return finish();
+    }
+    var task = tasks[index];
+    task(function () {
+        iterate(index + 1);
+    });
+}
+
+function finish() {
+    // iteration completed...
+}
+
+iterate(0);
